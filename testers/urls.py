4bc99c44a70d92d575005f421f45285f5f05141b
stass21 <stass21@rambler.ru>
2017-07-21 00:41:11 +0300

    1

--- testers/urls.py
+++ testers/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from testers import views
 #from django.conf import settings
 #from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^new_test/',include('new_test.urls')),
+    url(r'^$', views.welcome, name='welcome')
 ]
