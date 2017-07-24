a17dc3c0da583fcf1963c93602a5e27446633699
stass21 <stass21@rambler.ru>
2017-07-25 01:59:46 +0300

    2

--- new_test/models.py
+++ new_test/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Test(models.Model):
+    name = models.CharField(max_length=500)
+    theme = models.CharField(max_length=500)
+    question_1 = models.CharField(max_length=500, blank=True)
+
+
+
+class Question(models.Model):
+    test = models.ForeignKey(Test)
+    name = models.CharField(max_length=500)
+    answer_1 = models.CharField(max_length=100, blank=True)
+    answer_2 = models.CharField(max_length=100, blank=True)
+    answer_3 = models.CharField(max_length=100, blank=True)
