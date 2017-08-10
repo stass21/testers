54b86f3d76acf26e549ae422b0d72c5fc54806a1
stass21 <stass21@rambler.ru>
2017-08-10 23:48:46 +0300

    3

--- new_test/admin.py
+++ new_test/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from new_test.models import Test, Question
 # Register your models here.
+
+admin.site.register(Test)
+admin.site.register(Question)
+
+
--- new_test/models.py
+++ new_test/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here.
 
 class Test(models.Model):
     name = models.CharField(max_length=500)
     theme = models.CharField(max_length=500)
-    question_1 = models.CharField(max_length=500, blank=True)
+    result = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 
 
 class Question(models.Model):
     test = models.ForeignKey(Test)
     name = models.CharField(max_length=500)
-    answer_1 = models.CharField(max_length=100, blank=True)
-    answer_2 = models.CharField(max_length=100, blank=True)
-    answer_3 = models.CharField(max_length=100, blank=True)
+    answer_1 = models.CharField(max_length=100)
+    answer_2 = models.CharField(max_length=100)
+    answer_true = models.CharField(max_length=100)
+    result = models.IntegerField(default=0)
+
+    def true_answer(self,answer):
+        if self.answer_true == answer:
+            self.result += 1
+            return self.result
+
+
+
+
+    def __str__(self):
+        return self.name
